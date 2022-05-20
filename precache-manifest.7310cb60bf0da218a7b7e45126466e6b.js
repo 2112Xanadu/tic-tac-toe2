7019fd805fad51a280e782986959ee408cfb1982
Grete <greteveetla@gmail.com>
2022-05-20 16:45:58 +0300

    Updates

---
+++ precache-manifest.7310cb60bf0da218a7b7e45126466e6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3abad7d8526f79020783",
+    "url": "/tic-tac-toe2/static/js/main.a824e32d.chunk.js"
+  },
+  {
+    "revision": "00e8bfc2546cb7520e8e",
+    "url": "/tic-tac-toe2/static/js/runtime~main.a3e1ad7d.js"
+  },
+  {
+    "revision": "ba234d3429b3f8dcd997",
+    "url": "/tic-tac-toe2/static/js/2.88c00f9f.chunk.js"
+  },
+  {
+    "revision": "ab87758b515d0d605b274cb97e89f971",
+    "url": "/tic-tac-toe2/index.html"
+  }
+];
---
+++ static/js/main.a824e32d.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),a=t(2),c=function(){return r.a.createElement("div",null,"Game")};t.n(a).a.render(r.a.createElement(c,null),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.a824e32d.chunk.js.map
